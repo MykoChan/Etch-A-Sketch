044ed9c3e029c9149856dcfdb15b4b9902c1818c
Michael Chan <chan.myko@gmail.com>
2022-12-01 23:41:27 -0500

    Fix bug preventing mouseup event from firing by adding user-select: none Refactor/reorder js code

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,33 +1,46 @@
-const size = 5;
-
-let gridContainer = document.createElement('div');
-gridContainer.classList.add("grid-container")
-
-const body = document.querySelector("body");
-body.appendChild(gridContainer);
-
 const container = document.querySelector('.grid-container');
 const pencilButton = document.querySelector('#pencil');
 const eraserButton = document.querySelector('#eraser');
 const clearButton = document.querySelector('#clear');
 const colorPicker = document.querySelector('#color-picker');
+const canvasSizeText = document.querySelector('#canvas-size');
+const canvasSizeSlider = document.querySelector('#canvas-size-slider')
 
-pencilButton.classList.add("button-selected");
+// Default values
+const DEFAULT_COLOR = "black";
+const DEFAULT_MODE = "pencil"
+const DEFAULT_SIZE = 16;
 
+let color = DEFAULT_COLOR;
+let currentMode = DEFAULT_MODE;
+let canvasSize = DEFAULT_SIZE;
 let mousedown = false;
-let currentMode = "pencil";
-let color = "black";
 
-document.onmousedown = () => {
-    mousedown = true;
-}
-document.onmouseup = () => {
-    mousedown = false;
+// Event listeners
+colorPicker.addEventListener('input', setCurrentColor)
+pencilButton.addEventListener('click', setCurrentMode)
+eraserButton.addEventListener('click', setCurrentMode)
+clearButton.addEventListener('click', clearCanvas)
+canvasSizeSlider.addEventListener('click', updateCanvasSizeText)
+canvasSizeSlider.addEventListener('click', updateCanvasSize)
+
+document.body.onmousedown = () => (mousedown = true);
+document.body.onmouseup = () => (mousedown = false);
+
+function setupGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++) {
+        let pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixel.addEventListener('mousedown', draw)
+        pixel.addEventListener('mouseover', draw)
+        container.appendChild(pixel);
+    }
 }
 
 function draw(e) {
     if (e.type === 'mouseover' && !mousedown) return;
-    console.log(e.target);
     if (currentMode === "pencil") {
         e.target.style.backgroundColor = color;
     } else if (currentMode === "eraser") {
@@ -35,40 +48,33 @@ function draw(e) {
     }   
 }
 
-function setupGrid(size) {
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    for (let i = 0; i < size*size; i++) {
-        let pixel = document.createElement('div');
-        pixel.className = "pixel";
-        container.appendChild(pixel);
-    }
-
-    const pixels = document.querySelectorAll('.pixel'); 
-    pixels.forEach(pixel => pixel.addEventListener('mousedown', draw))
-    pixels.forEach(pixel => pixel.addEventListener('mouseover', draw))
+function setCurrentColor(e) {
+    color = e.target.value;
 }
 
-function clearCanvas() {
-    gridContainer.innerHTML = '';
-    setupGrid(size)
-}
-
-function changeMode(button) {
-    console.log(button.target);
+function setCurrentMode(button) {
     pencilButton.classList.remove("button-selected");
     eraserButton.classList.remove("button-selected");
     button.target.classList.add("button-selected");
     currentMode = button.target.id;
 }
 
-function changeColor(e) {
-    color = e.target.value;
+function clearCanvas() {
+    container.innerHTML = '';
+    setupGrid(canvasSize)
 }
 
-setupGrid(size);
+function updateCanvasSizeText(e) {
+    canvasSizeText.innerText = `${e.target.value} x ${e.target.value}`;
+}
 
-pencilButton.addEventListener('click', changeMode)
-eraserButton.addEventListener('click', changeMode)
-clearButton.addEventListener('click', clearCanvas)
-colorPicker.addEventListener('input', changeColor)
+function updateCanvasSize(e) {
+    canvasSize = e.target.value;
+    updateCanvasSizeText(e.target.value)
+    clearCanvas();
+}
+
+window.onload = () => {
+    pencilButton.classList.add("button-selected");
+    setupGrid(canvasSize);
+}
